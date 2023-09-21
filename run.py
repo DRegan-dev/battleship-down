51fb194271b5ccdb9f7a692ba470335de2eef62a
DRegan-dev <>
2023-09-21 21:42:07 +0000

    added function to create game board of a given size

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
 # Import the randint function from the random module
 from random import randint
+
+# Function to create a game board of a given size
+def create_board(b_size):
+    board = []
+    for i range(b_size):
+        board.append([0] * b_size)
+    return board
